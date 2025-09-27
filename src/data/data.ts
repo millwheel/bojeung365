01742ba35c893b8750e1502168d96d82af69afc7
sijun <sionwer5@gmail.com>
2025-09-27 21:39:13 +0900

    feat: type 추가

---
+++ src/data/data.ts
@@ -0,0 +1,50 @@
+export enum BoardType {
+    Notice = 'notice',
+    Event = 'event',
+    Review = 'review',
+    Report = 'report',
+    Tether = 'tether',
+    Official = 'official',
+}
+
+export type BasePost = {
+    id: number;
+    board: BoardType;
+    title: string;
+    content: string;
+    author: string;
+    createdAt: string; // ISO
+    views: number;
+};
+
+export type ReportPost = BasePost & {
+    id: number;
+    siteName: string;
+    siteUrl: string;
+}
+
+export type EventPost  = BasePost & {
+    board: BoardType.Event;
+    eventStart?: string;
+    eventEnd?: string;
+};
+
+export type ReviewPost = BasePost & {
+    board: BoardType.Review;
+    siteName: string;
+    siteUrl: string;
+    incomeDate: string;
+    incomeAmount: number;
+    velocityRating: number;
+    incomeRating: number;
+    eventRating: number;
+    reliabilityRating: number;
+};
+
+export type Comment = {
+    id: number;
+    postId: number;
+    author: string;
+    content: string;
+    createdAt: string;
+};
