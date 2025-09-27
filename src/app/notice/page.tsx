0e8f9cd0c35f5bc4fc55d1fc547e0fd97b0cc098
sijun <sionwer5@gmail.com>
2025-09-27 20:47:34 +0900

    feat: 배너 git 추가

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
 
 
 export default function BlackList() {
     return (
-        <section className="bg-[#22242d] h-72">
-            <span>공지사항 내용</span>
-        </section>
+        <div>
+            <section className="relative bg-[#22242d] h-60 text-white">
+                <div>
+                    <Image src="/image/banner.gif" alt="공식업체 배너" fill className="object-cover" />
+                </div>
+            </section>
+            <section>
+                <div className="py-2">
+                    <h2 className="text-base font-bold">공지사항</h2>
+                    <div className="border-b-2 border-red-500 w-full"></div>
+                </div>
+                <div>
+                    게시판
+                </div>
+            </section>
+        </div>
     );
 }
--- src/app/official/page.tsx
+++ src/app/official/page.tsx
@@ -1,9 +1,22 @@
-
+import Image from "next/image";
 
 export default function OfficialPage() {
     return (
-        <section className="bg-[#22242d] h-72">
-            <span>공식보증업체 내용</span>
-        </section>
+        <div>
+            <section className="relative bg-[#22242d] h-60 text-white">
+                <div>
+                    <Image src="/image/banner.gif" alt="공식업체 배너" fill className="object-cover" />
+                </div>
+            </section>
+            <section>
+                <div className="py-2">
+                    <h2 className="text-base font-bold">공식보증업체</h2>
+                    <div className="border-b-2 border-red-500 w-full"></div>
+                </div>
+                <div>
+                    게시판
+                </div>
+            </section>
+        </div>
     );
 }
