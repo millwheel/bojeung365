6ec4258559581d9204e468f25d92a0df7c1c9207
sijun <sionwer5@gmail.com>
2025-10-09 21:18:15 +0900

    comment: todo 주석 추가

--- src/component/authbox.tsx
+++ src/component/authbox.tsx
@@ -1,11 +1,17 @@
 import { supabaseServerClient } from "@/supabase/server";
 import Login from "@/component/login";
 import Profile from "@/component/profile";
+import apiClient from "@/lib/apiClient";
+import toast from "react-hot-toast";
+import axios from "axios";
+import {ApiError} from "@/type/errorType";
 
 export default async function AuthBox() {
 //     const supabase = await supabaseServerClient();
 //     const { data } = await supabase.auth.getSession();
 //     const session = data.session;
+
+    // TODO getMe 호출 로직 추가
 
     if (!session) {
         return <Login />;
